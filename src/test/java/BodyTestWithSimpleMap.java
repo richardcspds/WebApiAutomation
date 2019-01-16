be89e9c55607f57e6e8002b634bdf6cf6245c72b
Richard Cespedes <richardcspds@gmail.com>
2019-01-16 17:35:49 -0400

    initial commit

---
+++ src/test/java/BodyTestWithSimpleMap.java
@@ -0,0 +1,48 @@
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static entities.User.ID;
+import static entities.User.LOGIN;
+
+public class BodyTestWithSimpleMap  extends BaseClass{
+
+    @Test
+    public void returnsCorrectLogin() throws IOException {
+        HttpGet get = new HttpGet(BASE_ENDPOINT + "/users/andrejss88");
+
+        response = client.execute(get);
+
+        String jsonBody = EntityUtils.toString(response.getEntity());
+
+        JSONObject jsonObject = new JSONObject(jsonBody);
+
+        String loginValue = (String) getValueFor(jsonObject, LOGIN);
+
+        Assert.assertEquals(loginValue, "andrejss88");
+    }
+
+    @Test
+    public void returnsCorrectId() throws IOException {
+        HttpGet get = new HttpGet(BASE_ENDPOINT + "/users/andrejss88");
+
+        response = client.execute(get);
+
+        String jsonBody = EntityUtils.toString(response.getEntity());
+
+        JSONObject jsonObject = new JSONObject(jsonBody);
+
+        Integer loginValue = (Integer) getValueFor(jsonObject, ID);
+
+        Assert.assertEquals(loginValue, Integer.valueOf(11834443));
+    }
+
+    private Object getValueFor(JSONObject jsonObject, String key) {
+
+        return jsonObject.get(key);
+    }
+}
---
+++ src/test/java/Get200.java
@@ -0,0 +1,65 @@
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class Get200 extends BaseClass {
+
+    CloseableHttpClient client;
+    CloseableHttpResponse response;
+
+    @BeforeMethod
+    public void setup(){
+        client = HttpClientBuilder.create().build();
+    }
+
+    @AfterMethod
+    public void closeResources() throws IOException {
+        client.close();
+        response.close();
+    }
+
+    @Test
+    public void baseUrlReturns200() throws IOException {
+
+        HttpGet get = new HttpGet(BASE_ENDPOINT);
+
+        response = client.execute(get);
+
+        int actualStatus = response.getStatusLine().getStatusCode();
+
+        Assert.assertEquals(actualStatus, 200);
+    }
+
+    @Test
+    public void rateLimitUrlReturns200() throws IOException {
+
+        HttpGet get = new HttpGet(BASE_ENDPOINT + "/rate_limit");
+
+        response = client.execute(get);
+
+        int actualStatus = response.getStatusLine().getStatusCode();
+
+        Assert.assertEquals(actualStatus, 200);
+    }
+
+    @Test
+    public void searchReposReturns200() throws IOException {
+
+        HttpGet get = new HttpGet(BASE_ENDPOINT + "/search/repositories?q=java");
+
+        response = client.execute(get);
+
+        int actualStatus = response.getStatusLine().getStatusCode();
+
+        Assert.assertEquals(actualStatus, 200);
+    }
+}
